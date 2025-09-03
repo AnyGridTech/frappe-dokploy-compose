fd7870030b76279ab8110dce20933debde714144
troyaks <lgotcfg@gmail.com>
2025-09-03 15:01:54 -0300

    Fixing variable error on create_site_setup.sh

--- create_site_setup.sh
+++ create_site_setup.sh
@@ -8,20 +8,22 @@ echo "Running create_site_setup.sh"
 wait-for-it -t 120 db-service-test:3306
 wait-for-it -t 120 redis-cache-service-test:6379
 wait-for-it -t 120 redis-queue-service-test:6379
-export start=`date +%s`
-export max_wait_time=200
+
+start=$(date +%s)
+max_wait_time=200
 echo "Waiting for sites/common_site_config.json to be created"
 
-until [[ -n `grep -hs ^ sites/common_site_config.json | jq -r ".db_host // empty"` ]] &&
-  [[ -n `grep -hs ^ sites/common_site_config.json | jq -r ".redis_cache // empty"` ]] &&
-  [[ -n `grep -hs ^ sites/common_site_config.json | jq -r ".redis_queue // empty"` ]];
+until [[ -n $(grep -hs ^ sites/common_site_config.json | jq -r ".db_host // empty") ]] &&
+  [[ -n $(grep -hs ^ sites/common_site_config.json | jq -r ".redis_cache // empty") ]] &&
+  [[ -n $(grep -hs ^ sites/common_site_config.json | jq -r ".redis_queue // empty") ]];
 do
   sleep 2
-  if (( `date +%s` - start > $max_wait_time )); then
-    echo "could not find sites/common_site_config.json with required keys"
+  if (( $(date +%s) - start > max_wait_time )); then
+    echo "❌ could not find sites/common_site_config.json with required keys"
     exit 1
   fi
 done
+
 
 echo "sites/common_site_config.json found"
 echo "Creating new site..."
