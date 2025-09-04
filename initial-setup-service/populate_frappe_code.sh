0e0807c3bd5e6e18a55d4952d3379d7917908b49
troyaks <lgotcfg@gmail.com>
2025-09-04 10:58:42 -0300

    git still not being recognized, changing cp arguments to avoid it.

--- initial-setup-service/populate_frappe_code.sh
+++ initial-setup-service/populate_frappe_code.sh
@@ -7,7 +7,7 @@ echo "Running populate_frappe_code.sh..."
 echo "🔎 Checking if bind is already populated..."
 if [ ! -d /mnt/apps/frappe ] || [ ! -d /mnt/apps/erpnext ]; then
   echo "📦 Populating /mnt/apps from image..."
-  cp -a /home/frappe/frappe-bench/apps/* /mnt/apps/
+  cp -a /home/frappe/frappe-bench/apps/. /mnt/apps/
   chown -R 1000:1000 /mnt/apps || true
   echo "✅ apps/ populated."
 else
